a81a9883320f87871eacdba6a2632f2e7c5f318f
Thomas Sheffler <tom.sheffler@gmail.com>
2025-07-21 16:05:37 -0700

    utilities for handling NLIP messages, convertion to/from app message domain model

---
+++ utils.py
@@ -0,0 +1,78 @@
+#
+# Utilities for converting message types
+#
+
+from kivy_chat_app import Message
+from nlip_sdk.nlip import NLIP_Message, NLIP_Factory
+
+import os
+import tempfile
+from base64 import b64encode, b64decode
+
+def messageToNlipMessage(message: Message):
+
+    # every message has text content
+    content = message.content
+    nlip_message = NLIP_Factory.create_text(content=content)
+    
+
+    if message.message_type == "image":
+        image_path = message.image_path
+        root, extension = os.path.splitext(image_path)
+        basename = os.path.basename(image_path) # remove directory part and use as label
+        extension = extension.replace(".", "") # remove any period
+
+        fp = open(image_path, "rb")
+        data = fp.read()
+        fp.close()
+
+        b64content=b64encode(data).decode("utf-8")
+        nlip_message.add_binary(b64content, "image", extension, label=basename)
+
+    return nlip_message
+
+#
+# Find a submessage with an image and store to an image path or return None
+#
+
+def nlipMessageExtractImagePath(nlip_message: NLIP_Message):
+
+    temp_file_path = None
+
+    if hasattr(nlip_message, 'submessages'):
+        if (nlip_message.submessages is not None):
+            for submsg in nlip_message.submessages:
+                subformat = submsg.subformat
+
+                # subformat="image/{encoding}
+                if (subformat.startswith("image")):
+
+                    # is assumed base64 encoded string                    
+                    content = submsg.content
+                    kind, encoding = subformat.split("/")
+
+                    # base64 decode it
+                    data = b64decode(content.encode('utf-8'))
+
+                    # write it to a temp file
+                    tf = tempfile.NamedTemporaryFile(suffix=encoding, mode='wb', delete=False)
+                    tf.write(data)
+                    tf.close()
+                    temp_file_path = tf.name
+
+                    break
+    return temp_file_path
+                
+
+#
+# Find text content in primary message and the first attached image.  Return
+# a local file path for the saved image that can be used with Kivy.
+#
+
+def nlipMessageExtractParts(nlip_message: NLIP_Message):
+
+    # find some text in the message
+    content = nlip_message.extract_text()
+    image_path = nlipMessageExtractImagePath(nlip_message)
+
+    return (content, image_path)
