b5657fb3ff2ea5095765d962010732bbe08769bd
Thomas Sheffler <tom.sheffler@gmail.com>
2025-09-15 11:46:37 -0700

    models: add formatted option to Message

--- mach2/models.py
+++ mach2/models.py
@@ -9,6 +9,7 @@ class Message:
     """Domain entity: Represents a chat message"""
     id: str
     content: str
+    formatted: str  # kivy markup version of content if present
     message_type: str  # "text" or "image"
     image_path: Optional[str] = None
     role: str = "user" # user, assistant, system, status
