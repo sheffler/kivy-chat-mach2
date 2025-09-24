b3e76769e3425df047e1f025b457b8c999954337
Thomas Sheffler <tom.sheffler@gmail.com>
2025-09-24 16:04:23 -0700

    Make use of SHIFT-RETURN like other chat interfaces.

--- mach2/widgets/text_input_with_shift_return.py
+++ mach2/widgets/text_input_with_shift_return.py
@@ -8,10 +8,10 @@ from kivy.uix.textinput import TextInput
 class TextInputWithShiftReturn(TextInput):
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
         # Check if SHIFT is pressed and the key is RETURN
-        if 'shift' in modifiers and keycode[1] == 'enter':
+        if keycode[1] == 'enter' and 'shift' not in modifiers:
             self.dispatch('on_text_validate')
             return True  # Consume the event
-        # If not SHIFT-RETURN, let the default TextInput behavior handle it
+        # If is SHIFT-RETURN, let the default TextInput behavior handle it
         return super().keyboard_on_key_down(window, keycode, text, modifiers)
 
 
